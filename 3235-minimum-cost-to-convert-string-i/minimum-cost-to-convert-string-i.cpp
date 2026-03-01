6358d77723bea373a55be94328ea9588e79fa698
Naman Saxena <namansaxena2029@gmail.com>
2026-03-01 14:03:38 +0530

    Time: 23 ms (75.07%) | Memory: 100.2 MB (62.01%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, 
+                           vector<char>& original, 
+                           vector<char>& changed, 
+                           vector<int>& cost) {
+        
+        int n = source.length();
+        if(source.length() != target.length())
+            return -1;
+        const long long INF = 1e18;
+        vector<vector<long long>> dist(26, vector<long long>(26, INF));
+        for(int i = 0; i < 26; i++)
+            dist[i][i] = 0;
+        for(int i = 0; i < original.size(); i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = min(dist[u][v], (long long)cost[i]);
+        }
+        for(int k = 0; k < 26; k++) {
+            for(int i = 0; i < 26; i++) {
+                for(int j = 0; j < 26; j++) {
+                    if(dist[i][k] != INF && dist[k][j] != INF)
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        long long ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(source[i] == target[i])
+                continue;
+            int u = source[i] - 'a';
+            int v = target[i] - 'a';
+            if(dist[u][v] == INF)
+                return -1;            
+            ans += dist[u][v];
+        }
+        return ans;
+    }
+};
