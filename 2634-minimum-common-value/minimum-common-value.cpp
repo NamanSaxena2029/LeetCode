b263aa66c27998170b40ce289ef28c17410b3a0d
Naman Saxena <namansaxena2029@gmail.com>
2026-05-19 10:12:21 +0530

    Time: 48 ms (11.64%) | Memory: 69.6 MB (14.31%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0 , j = 0;
-        while(i<nums1.size() && j<nums2.size()) {
-            if(nums1[i]==nums2[j]) {
-                return nums1[i];
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+/* Using 2 pointer */        
+        // int i = 0 , j = 0;
+        // while(i<nums1.size() && j<nums2.size()) {
+        //     if(nums1[i]==nums2[j]) {
+        //         return nums1[i];
+        //     } else if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
+        // return -1;
+
+/* Using set */
+        unordered_set<int> mp;
+        for(int i : nums1) {
+            mp.insert(i);
+        }
+        for(int i : nums2) {
+            if(mp.find(i) != mp.end()) {
+                return i;
             }
         }
         return -1;
