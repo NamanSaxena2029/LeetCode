440ca3bda343cb5245d57798cf648483a9de1250
Naman Saxena <namansaxena2029@gmail.com>
2026-05-20 09:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 85.7 MB (85.47%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        unordered_map<int, int> mp;
-        int common  = 0;
+        // unordered_map<int, int> mp;
+        // int common  = 0;
+        // vector<int> arr(A.size());
+        // for(int i = 0 ; i < A.size() ; i++) {
+        //     mp[A[i]]++;
+        //     if(mp[A[i]]==2) {
+        //         common++;
+        //     }
+        //     mp[B[i]]++;
+        //     if(mp[B[i]]==2) {
+        //         common++;
+        //     }
+        //     arr[i] = common;
+        // }
+        // return arr;
+
+/* Frequency Array*/
+        vector<int> freq(A.size()+1, 0);
         vector<int> arr(A.size());
+        int common = 0;
         for(int i = 0 ; i < A.size() ; i++) {
-            mp[A[i]]++;
-            if(mp[A[i]]==2) {
+            freq[A[i]]++;
+            if(freq[A[i]]==2) {
                 common++;
             }
-            mp[B[i]]++;
-            if(mp[B[i]]==2) {
+            freq[B[i]]++;
+            if(freq[B[i]]==2) {
                 common++;
             }
             arr[i] = common;
