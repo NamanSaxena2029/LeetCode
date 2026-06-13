1b89bd681a19adac0c8fedc684b3067251d0989d
Naman Saxena <namansaxena2029@gmail.com>
2026-06-13 17:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (60.84%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        for(int i : arr) {
+            mp[i]++;
+        }
+        unordered_set<int> st;
+        for(auto i : mp) {
+            if(st.count(i.second)) {
+                return false;
+            }
+            st.insert(i.second);
+        }
+        return true;
+    }
+};
