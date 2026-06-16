860c4e6c3695a17fc6dafa62d81b5a5116762213
Naman Saxena <namansaxena2029@gmail.com>
2026-06-16 10:19:14 +0530

    Time: 3 ms (76.89%) | Memory: 39.6 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string s1;
+        for(int i = 0 ; i  < s.size() ; i++) {
+            char ch = s[i];
+            if(ch=='*')  {
+                if(s1.size()!=0) {
+                    s1.pop_back();
+                }
+            } else if (ch=='#') {
+                s1 += s1;
+            } else if (ch=='%') {
+                reverse(s1.begin(),s1.end());
+            } else {
+                s1.push_back(ch);
+            }
+        }
+        return s1;
+    }
+};
